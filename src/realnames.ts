ea46c9130df1272e3d035fd9a119b023f332cc7f
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-01-20 23:10:02 -0500

    Add Correct name for King Slayer

--- src/realnames.ts
+++ src/realnames.ts
@@ -1,6 +1,6 @@
 const playerNames: { [key: string]: string } = {
   pawnmatedpcm: "Jayden Foreshow",
-  theslayertoken456: "Taniel Bennett",
+  theslayertoken456: "Taniel Barrett",
   daddystrength: "Raheem Glaves",
   samchesstion: "Samuel Smith",
   logic_lodge: "Odane Lodge",
